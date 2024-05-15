01b689f27a83ca8006f82c91a3b1428ed2e2c910
stvarvara <123358404+stvarvara@users.noreply.github.com>
2024-05-15 09:22:09 +0200

    Delete html directory

--- html/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    require "../utils.php";
-
-    $query = "SELECT * FROM sae._compte_client 
-    INNER JOIN sae._utilisateur ON sae._compte_client.id = sae._utilisateur.id
-    INNER JOIN sae._adresse ON sae._utilisateur.id_adresse = sae._adresse.id
-    LIMIT 1";
-    var_dump(request($query));
-?>
--- html/js/verifier.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('input[name="daterange"]').daterangepicker({
-      opens: 'right',
-      "startDate": "05/13/2024",
-    }, function(start, end, label) {
-      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    });
-  });
