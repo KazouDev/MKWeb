2f86ea7d386dc6f774fb96fc6661a169536e1e41
emoaligou <emoaligou@etudiant.univ-rennes1.fr>
2024-05-16 10:40:01 +0200

    suite consulter compte

--- html/consulter_compte_client.php
+++ html/consulter_compte_client.php
@@ -9,5 +9,21 @@
     inner join sae._adresse on sae._adresse.id = sae._utilisateur.id_adresse
     where sae._utilisateur.id = $id_utilisateur;";
     $rep_utilisateur = request($query_utilisateur, true);
-    echo $rep_utilisateur;
+    
+    $prenom = $rep_utilisateur['prenom'];
+    $nom = $rep_utilisateur['nom'];
+    $ville = $rep_utilisateur['ville'];
+    $pays = $rep_utilisateur['pays'];
+
+    if ($rep_utilisateur['civilite'] = "Mr"){
+        $genre = "Homme";
+    } else if ($rep_utilisateur['civilite'] = "Mme"){
+        $genre = "Femme";
+    } else {
+        $genre = "Autre";
+    }
+    $src_photo = $rep_utilisateur['photo_profile'];
+
+    
+
 ?>
